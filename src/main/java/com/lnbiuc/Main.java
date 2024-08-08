cc47119dfebc154f3ffaf773f6af9644b04c11d9
violet <hi@lnbiuc.copm>
2024-08-08 18:17:03 +0800

    update

--- src/main/java/com/lnbiuc/Main.java
+++ src/main/java/com/lnbiuc/Main.java
@@ -1,17 +1,21 @@
 package com.lnbiuc;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, InvalidFormatException {
+        final String wordFilePath = "sign.docx";
+        final String imageFilePath = "IMG_20210319_001710.jpg";
+        final String outputFilePath = "sign_handled.docx";
+
+        InputStream word = Main.class.getClassLoader().getResourceAsStream(wordFilePath);
+        InputStream image = Main.class.getClassLoader().getResourceAsStream(imageFilePath);
+
+        WordPictureReplacer.replaceBookmarkWithImage(word, image, outputFilePath, "sign");
     }
 }
--- src/main/java/com/lnbiuc/WordPictureReplacer.java
+++ src/main/java/com/lnbiuc/WordPictureReplacer.java
@@ -1,4 +1,15 @@
 package com.lnbiuc;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.util.Units;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTBookmark;
+
+import java.io.*;
+import java.util.List;
+
 /**
  * @Description: TODO
  * @Date: 2024/8/8 16:58
@@ -6,5 +17,55 @@ package com.lnbiuc;
  */
 
 public class WordPictureReplacer {
+    /**
+     * 在 Word 文档中的书签位置插入图片，并保存修改后的文档。
+     *
+     * @param wordFile     原始 Word 文档路径
+     * @param imageFile    图片文件路径
+     * @param outputFilePath   修改后的 Word 文档保存路径
+     * @return 修改后的 Word 文件
+     * @throws IOException      IO 异常
+     * @throws InvalidFormatException 格式异常
+     */
+    public static File replaceBookmarkWithImage(InputStream wordFile, InputStream imageFile, String outputFilePath, String bookmarkName) throws IOException, InvalidFormatException {
+        byte[] imageBytes = readImageFile(imageFile);
 
+        try (XWPFDocument doc = new XWPFDocument(wordFile)) {
+            for (XWPFParagraph paragraph : doc.getParagraphs()) {
+                List<CTBookmark> bookmarkStartList = paragraph.getCTP().getBookmarkStartList();
+                for (CTBookmark bookmark : bookmarkStartList) {
+                    if (bookmarkName.equals(bookmark.getName())) {
+                        List<XWPFRun> runs = paragraph.getRuns();
+                        XWPFRun run;
+                        if (!runs.isEmpty()) {
+                            run = runs.get(0);
+                        } else {
+                            run = paragraph.createRun();
+                        }
+                        run.addPicture(new ByteArrayInputStream(imageBytes), XWPFDocument.PICTURE_TYPE_PNG, "imageName.png", Units.toEMU(80), Units.toEMU(80));
+                    }
+                }
+            }
+
+            File outputFile = new File(outputFilePath);
+            try (FileOutputStream out = new FileOutputStream(outputFile)) {
+                doc.write(out);
+            }
+
+            return outputFile;
+        } catch (InvalidFormatException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static byte[] readImageFile(InputStream file) throws IOException {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = file.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            return outputStream.toByteArray();
+        }
+    }
 }
